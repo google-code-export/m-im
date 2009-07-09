6fc2848bdbbbc9a7957181201cb469b099459532
chunlinyao <chunlinyao@d12a5ce6-1927-11de-85e6-8b0190298422>
2009-07-09 03:20:06 +0000

    support CJK split

--- m-im/src/org/rost/mobile/guilib/core/TextSplitter.java
+++ m-im/src/org/rost/mobile/guilib/core/TextSplitter.java
@@ -17,9 +17,13 @@ import javax.microedition.lcdui.Font;
  */
 public class TextSplitter {
 
+    private static boolean canSplit(char c, char nextc) {
+        return (nextc < 0 || nextc > 127) || (c < 0 || c > 127) || delims.indexOf(c) > -1;
+    }
+
     public TextSplitter() {
     }
-    static char[] delims = {',', '.', '\n', ':', '-', '+', ';', '!', '?', ' '};
+    static String delims = ",.\n:-+;!? ";
 
     /*
      * This method requires a unit test as it's throwing lots of ArrayIndexOutOfBoundsExceptions in the Nokia 6230i emulator
@@ -28,43 +32,39 @@ public class TextSplitter {
         Vector v = new Vector();
         int lastIndex = 0;
         for (int i = 0; i < str.length(); i++) {
-            for (int j = 0; j < delims.length; j++) {
-                try {
-                    if (str.charAt(i) == delims[j]) {
-                        int k = i + 1;
-                        if (k < str.length()) {
-                            while (str.charAt(k) == ' ' && k < str.length()) {
+            try {
+                final char currentChar = str.charAt(i);
+                final char nextChar = i + 1 < str.length() ? str.charAt(i + 1) : '\0';
+                if (canSplit(currentChar, nextChar) || i == str.length() - 1) {
+                    int k = i + 1;
+                    if (k < str.length()) {
+                        while (str.charAt(k) == ' ' && k < str.length()) {
+                            k++;
+                        }
+                    }
+                    String slice = str.substring(lastIndex, k);
+                    lastIndex = k;
+                    i = k;
+                    if (font.stringWidth(slice) > width) {
+                        while (font.stringWidth(slice) > width) {
+                            k = 0;
+                            while (font.substringWidth(slice, 0, k) < width) {
                                 k++;
                             }
+                            String subSlice = slice.substring(0, k - 1);
+                            //                            System.out.println("subSlice = ["+subSlice+"]");
+                            v.addElement(subSlice);
+                            slice = slice.substring(k - 1);
                         }
-                        String slice = str.substring(lastIndex, k);
-                        lastIndex = k;
-                        i = k;
-                        if (font.stringWidth(slice) > width) {
-                            while (font.stringWidth(slice) > width) {
-                                k = 0;
-                                while (font.substringWidth(slice, 0, k) < width) {
-                                    k++;
-                                }
-                                String subSlice = slice.substring(0, k - 1);
-                                //                            System.out.println("subSlice = ["+subSlice+"]");
-                                v.addElement(subSlice);
-                                slice = slice.substring(k - 1);
-                            }
-                        }
-                        //                    System.out.println("slice = ["+slice+"]");
-                        v.addElement(slice);
                     }
-                } catch (Throwable t) {
+                    //                    System.out.println("slice = ["+slice+"]");
+                    v.addElement(slice);
+                }
+            } catch (Throwable t) {
 //		    t.printStackTrace();
                 }
 
-            }
         }
-        String slice = str.substring(lastIndex);
-//        System.out.println("slice = ["+slice+"]");
-        v.addElement(slice);
-
         return v;
     }
 }
